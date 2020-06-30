85db8341c38a9930ef3ea4e633adac2a43eaf1cb
nakul <58786560+nakul1010@users.noreply.github.com>
2020-06-30 19:04:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import xgboost
+import numpy as np
+import pandas as pd
+# Load the Random Forest CLassifier model
+#filename = 'xg_boost_regression_model.sav'
+loaded_model = pickle.load(open('weights/linear_regression.sav', 'rb'))
+
+app = Flask(__name__)
+    
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        b=[]
+        b.append(float(request.form['avg-T']))
+        b.append(float(request.form['max-T']))
+        b.append(float(request.form['min-T']))
+        b.append(float(request.form['slp']))
+        b.append(float(request.form['h']))
+        b.append(float(request.form['vv']))
+        b.append(float(request.form['v']))
+        b.append(float(request.form['vm']))
+    
+        a = ['T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM']
+        b = pd.DataFrame(b)
+        b = b.transpose()
+        for i in range(0,8):
+        		b = b.rename(columns={i : a[i]})
+    
+        my_prediction = int(loaded_model.predict(b))
+        #my_prediction = int(my_prediction)[1:-1] 
+
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
